25c51e94f5b2f87a4d06285e4e4337fbffe41689
Stone Liu <liu.sto@northeastern.edu>
2025-02-16 15:21:02 -0500

    Blazingly fast ui

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = { enabled = false },
+      scratch = { enabled = false },
+      terminal = { enabled = false },
+      scroll = { enabled = false },
+      indent = { enabled = false },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+              { find = "Choose type .* to import" }, -- jdtls organize imports
+            },
+          },
+          view = "mini",
+        },
+      },
+      cmdline = {
+        view = "cmdline",
+        format = {
+          cmdline = { pattern = "^:", icon = "", lang = "vim" },
+        },
+      },
+      popupmenu = {
+        enabled = false,
+      },
+      views = {
+        mini = { win_options = { winblend = 0 } },
+      },
+    },
+  },
+}
