d8160e7672a384325315ade3a7f7c9f5c40d7b8c
Stone Liu <liu.sto@northeastern.edu>
2025-03-19 16:01:35 -0400

    Update neovim colors to be the same as my terminal

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -77,7 +77,11 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-      style = "night"
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
 }
