92183516b318d4b5dc15765a061bd48b9d3a8a12
Stone Liu <liu.sto@northeastern.edu>
2025-05-24 18:01:46 -0400

    Wiki

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -16,4 +16,13 @@ return {
       { "<Leader>tm", "<cmd>TableModeToggle<cr>", desc = "Toggle Table Mode" },
     },
   },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 }
--- nvim/.config/nvim/lua/plugins/wiki.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "lervag/wiki.vim",
-  -- tag = "v0.10", -- uncomment to pin to a specific release
-  init = function()
-    -- wiki.vim configuration goes here, e.g.
-    vim.g.wiki_root = "~/wiki" -- Set your wiki root directory
-  end,
-}
