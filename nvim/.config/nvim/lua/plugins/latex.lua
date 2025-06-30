eea740f4e324b345108bbd96732e645b8bcb35e7
Stone Liu <liu.sto@northeastern.edu>
2025-06-30 07:10:20 -0400

    Add skim over typical pdf viewer

--- nvim/.config/nvim/lua/plugins/latex.lua
+++ nvim/.config/nvim/lua/plugins/latex.lua
@@ -16,8 +16,29 @@ return {
     lazy = false, -- we don't want to lazy load VimTeX
     -- tag = "v2.15", -- uncomment to pin to a specific release
     init = function()
-      -- VimTeX configuration goes here, e.g.
-      vim.g.vimtex_view_method = "general"
+      vim.g.vimtex_view_method = "skim"
+      vim.g.vimtex_view_skim_sync = 1
+      vim.g.vimtex_view_skim_activate = 1
+
+      -- Enable continuous compilation
+      vim.g.vimtex_compiler_latexmk = {
+        continuous = 1,
+        build_dir = "",
+        callback = 1,
+        executable = "latexmk",
+        options = {
+          "-verbose",
+          "-file-line-error",
+          "-synctex=1",
+          "-interaction=nonstopmode",
+        },
+      }
+
+      -- Optional: disable some default mappings if they conflict
+      vim.g.vimtex_mappings_enabled = 1
+
+      -- Optional: set compiler method
+      vim.g.vimtex_compiler_method = "latexmk"
     end,
   },
 }
