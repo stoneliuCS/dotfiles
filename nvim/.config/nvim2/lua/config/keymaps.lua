62044b2f026c972f26d2a0fad1bd1d285b55e544
Stone Liu <liu.sto@northeastern.edu>
2025-10-19 17:46:20 -0400

    Better LSP support using fzf-lua

--- nvim/.config/nvim2/lua/config/keymaps.lua
+++ nvim/.config/nvim2/lua/config/keymaps.lua
@@ -1,3 +1,4 @@
 -- Global Keymaps
 local map = vim.keymap.set
+-- Buffer keymaps
 map("n", "<leader>bb", "<cmd>b#<cr>", { desc = "Previous buffer" })
--- nvim/.config/nvim2/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim2/lua/plugins/fzf-lua.lua
@@ -11,6 +11,35 @@ return {
 				["shift-tab"] = "up",
 			},
 		},
+		oldfiles = {
+			-- In Telescope, when I used <leader>fr, it would load old buffers.
+			-- fzf lua does the same, but by default buffers visited in the current
+			-- session are not included. I use <leader>fr all the time to switch
+			-- back to buffers I was just in. If you missed this from Telescope,
+			-- give it a try.
+			include_current_session = true,
+		},
+		previewers = {
+			builtin = {
+				-- fzf-lua is very fast, but it really struggled to preview a couple files
+				-- in a repo. Those files were very big JavaScript files (1MB, minified, all on a single line).
+				-- It turns out it was Treesitter having trouble parsing the files.
+				-- With this change, the previewer will not add syntax highlighting to files larger than 100KB
+				-- (Yes, I know you shouldn't have 100KB minified files in source control.)
+				syntax_limit_b = 1024 * 100, -- 100KB
+			},
+		},
+		grep = {
+			-- One thing I missed from Telescope was the ability to live_grep and the
+			-- run a filter on the filenames.
+			-- Ex: Find all occurrences of "enable" but only in the "plugins" directory.
+			-- With this change, I can sort of get the same behaviour in live_grep.
+			-- ex: > enable --*/plugins/*
+			-- I still find this a bit cumbersome. There's probably a better way of doing this.
+			rg_glob = true, -- enable glob parsing
+			glob_flag = "--iglob", -- case insensitive globs
+			glob_separator = "%s%-%-", -- query separator pattern (lua): ' --'
+		},
 	},
 	keys = {
 		{
@@ -19,6 +48,7 @@ return {
 				require("fzf-lua").files()
 			end,
 			desc = "Find files",
+			nowait = true,
 		},
 		{
 			"<leader>g",
@@ -26,13 +56,31 @@ return {
 				require("fzf-lua").live_grep_native()
 			end,
 			desc = "Live grep (cwd)",
+			nowait = true,
 		},
 		{
-			"<leader>b",
+			"<leader>,",
 			function()
 				require("fzf-lua").buffers()
 			end,
-			desc = "Find current buffers",
+			desc = "Buffer search",
+			nowait = true,
+		},
+		{
+			"gd",
+			function()
+				require("fzf-lua").lsp_definitions()
+			end,
+			desc = "Lsp definitions",
+			nowait = true,
+		},
+		{
+			"gr",
+			function()
+				require("fzf-lua").lsp_references()
+			end,
+			desc = "Lsp references",
+			nowait = true,
 		},
 	},
 }
