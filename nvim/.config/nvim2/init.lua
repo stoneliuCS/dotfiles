012bc540cf6ce9d225ca3f380e40ad521283b6f2
Stone Liu <liu.sto@northeastern.edu>
2025-10-19 14:05:13 -0400

    Configure based-pyright, autosessions

--- nvim/.config/nvim2/init.lua
+++ nvim/.config/nvim2/init.lua
@@ -8,11 +8,13 @@ vim.cmd([[colorscheme rose-pine-main]])
 -- Load the lsps
 vim.lsp.config("luals", require("config.lsp.luals"))
 vim.lsp.config("texlab", require("config.lsp.texlab"))
+vim.lsp.config("basedpyright", require("config.lsp.basedpyright"))
 
 -- Enable LSPs
 vim.lsp.enable({
 	"luals",
 	"texlab",
+	"basedpyright",
 })
 
 -- Diagnostic Configuration
