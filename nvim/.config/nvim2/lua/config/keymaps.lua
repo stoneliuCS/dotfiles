d6517bf84941e07f1e7a06651b5840fec7bd6e04
Stone Liu <liu.sto@northeastern.edu>
2025-11-02 13:11:28 -0500

    Add buffer delete keymap

--- nvim/.config/nvim2/lua/config/keymaps.lua
+++ nvim/.config/nvim2/lua/config/keymaps.lua
@@ -2,5 +2,6 @@
 local map = vim.keymap.set
 -- Buffer keymaps
 map("n", "<leader>bb", "<cmd>b#<cr>", { desc = "Previous buffer" })
+map("n", "<leader>bd", "<cmd>bd<cr>", { desc = "Delete current buffer" })
+-- Code Actions
 map("n", "cd", "<cmd>lua vim.diagnostic.open_float()<CR>" , { desc = "Show code diagnostics" })
-
