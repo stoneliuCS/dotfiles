90ce6d3d0acc118f43891f847d886122ebbf2d47
Stone Liu <liu.sto@northeastern.edu>
2025-11-02 13:34:11 -0500

    Formatting and adding markdown preview plugin

--- nvim/.config/nvim2/lua/plugins/format.lua
+++ nvim/.config/nvim2/lua/plugins/format.lua
@@ -1,37 +1,43 @@
 -- lua/plugins/conform.lua
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  cmd = { "ConformInfo" },
-  keys = {
-    { "<leader>cf", function() require("conform").format({ async = false, lsp_fallback = true }) end, desc = "Format file" },
-  },
-  opts = {
-    notify_on_error = false,
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>cf",
+			function()
+				require("conform").format({ async = false, lsp_fallback = true })
+			end,
+			desc = "Format file",
+		},
+	},
+	opts = {
+		notify_on_error = false,
 
-    -- Choose formatters per filetype (first available wins)
-    formatters_by_ft = {
-      lua             = { "stylua" },
+		-- Choose formatters per filetype (first available wins)
+		formatters_by_ft = {
+			lua = { "stylua" },
 
-      javascript      = { "prettierd", "prettier" },
-      typescript      = { "prettierd", "prettier" },
-      javascriptreact = { "prettierd", "prettier" },
-      typescriptreact = { "prettierd", "prettier" },
-      vue             = { "prettierd", "prettier" },
-      svelte          = { "prettierd", "prettier" },
-      json            = { "prettierd", "jq" },
-      jsonc           = { "prettierd" },
-      yaml            = { "prettierd" },
-      html            = { "prettierd" },
-      css             = { "prettierd" },
-      scss            = { "prettierd" },
-      markdown        = { "prettierd" },
-      graphql         = { "prettierd" },
+			javascript = { "prettierd", "prettier" },
+			typescript = { "prettierd", "prettier" },
+			javascriptreact = { "prettierd", "prettier" },
+			typescriptreact = { "prettierd", "prettier" },
+			vue = { "prettierd", "prettier" },
+			svelte = { "prettierd", "prettier" },
+			json = { "prettierd", "jq" },
+			jsonc = { "prettierd" },
+			yaml = { "prettierd" },
+			html = { "prettierd" },
+			css = { "prettierd" },
+			scss = { "prettierd" },
+			markdown = { "prettierd" },
+			graphql = { "prettierd" },
 
-      sh              = { "shfmt" },
-      python          = { "ruff_format", "black" },
-      go              = { "goimports", "gofmt" },
-      rust            = { "rustfmt" },
-    },
-  },
+			sh = { "shfmt" },
+			python = { "ruff_format", "black" },
+			go = { "goimports", "gofmt" },
+			rust = { "rustfmt" },
+		},
+	},
 }
--- nvim/.config/nvim2/lua/plugins/tree-sitter.lua
+++ nvim/.config/nvim2/lua/plugins/tree-sitter.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    lazy = false, -- Load immediately instead of using events
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "css", "python", "markdown" },
-        auto_install = true,
-        highlight = {
-          enable = true,
-          disable = { "latex" },
-          additional_vim_regex_highlighting = false,
-        },
-        indent = { enable = true },
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		lazy = false, -- Load immediately instead of using events
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "css", "python", "markdown" },
+				auto_install = true,
+				highlight = {
+					enable = true,
+					disable = { "latex" },
+					additional_vim_regex_highlighting = false,
+				},
+				indent = { enable = true },
+			})
+		end,
+	},
 }
