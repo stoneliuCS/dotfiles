bf163c52eaea1b10f2ec3b8c48c9e037a16c2d22
Stone Liu <liu.sto@northeastern.edu>
2025-11-03 19:05:01 -0500

    Fix: Increase tex lab formatting lsp timeouts on large files

--- nvim/.config/nvim2/lua/config/lsp/texlab.lua
+++ nvim/.config/nvim2/lua/config/lsp/texlab.lua
@@ -1,4 +1,14 @@
 return {
 	cmd = { "texlab" },
 	filetypes = { "tex" },
+	settings = {
+		texlab = {
+			-- Increase timeout for formatting
+			latexFormatter = "latexindent",
+			latexindent = {
+				["local"] = nil,
+				modifyLineBreaks = false,
+			},
+		},
+	},
 }
