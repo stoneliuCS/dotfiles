34bc2d1ba6ee65c4258a30a3b10f0c8ed039ce87
Stone Liu <liu.sto@northeastern.edu>
2025-11-03 19:08:51 -0500

    Stay in visual mode after highlight shifting

--- nvim/.config/nvim2/lua/config/keymaps.lua
+++ nvim/.config/nvim2/lua/config/keymaps.lua
@@ -4,4 +4,7 @@ local map = vim.keymap.set
 map("n", "<leader>bb", "<cmd>b#<cr>", { desc = "Previous buffer" })
 map("n", "<leader>bd", "<cmd>bd<cr>", { desc = "Delete current buffer" })
 -- Code Actions
-map("n", "cd", "<cmd>lua vim.diagnostic.open_float()<CR>" , { desc = "Show code diagnostics" })
+map("n", "cd", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Show code diagnostics" })
+-- Stay in visual mode after re-indenting
+map("v", "<", "<gv", { noremap = true, silent = true })
+map("v", ">", ">gv", { noremap = true, silent = true })
