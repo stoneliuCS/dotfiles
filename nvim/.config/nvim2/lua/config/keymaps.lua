889a82095991e77adb980e9d3c466a8947aa8eba
Stone Liu <liu.sto@northeastern.edu>
2025-11-09 20:55:39 -0500

    Add terminal esc key map

--- nvim/.config/nvim2/lua/config/keymaps.lua
+++ nvim/.config/nvim2/lua/config/keymaps.lua
@@ -1,10 +1,16 @@
 -- Global Keymaps
 local map = vim.keymap.set
--- Buffer keymaps
+-- Normal Mode Keymaps
+-- Buffer Mode Keymaps
 map("n", "<leader>bb", "<cmd>b#<cr>", { desc = "Previous buffer" })
 map("n", "<leader>bd", "<cmd>bd<cr>", { desc = "Delete current buffer" })
--- Code Actions
+-- Code Keymaps
 map("n", "cd", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Show code diagnostics" })
+
+-- Visual Mode Keymaps
 -- Stay in visual mode after re-indenting
 map("v", "<", "<gv", { noremap = true, silent = true })
 map("v", ">", ">gv", { noremap = true, silent = true })
+
+-- Terminal Mode Keymaps
+map('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
