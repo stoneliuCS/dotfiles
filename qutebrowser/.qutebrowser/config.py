d2d25f8ea71c1d7cf46f96c9b66fb0ddf8785a4d
Stone Liu <liu.sto@northeastern.edu>
2025-12-17 14:44:52 -0500

    Add qute browser config

---
+++ qutebrowser/.qutebrowser/config.py
@@ -0,0 +1,7 @@
+config.load_autoconfig()
+c.url.searchengines = {
+  "DEFAULT": "https://www.google.com/search?q={}",
+  "g": "https://www.google.com/search?q={}",
+}
+c.url.default_page = "https://www.google.com"          # page on new windows/tabs
+c.url.start_pages = ["https://www.google.com"]         # pages opened at startup
