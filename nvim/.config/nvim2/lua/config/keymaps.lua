62b65d5a600ee1dcacad7971b0341209ee17fbd8
Stone Liu <liu.sto@northeastern.edu>
2025-12-22 20:00:27 -0500

    Add shortcut for moving between windows

--- nvim/.config/nvim2/lua/config/keymaps.lua
+++ nvim/.config/nvim2/lua/config/keymaps.lua
@@ -1,16 +1,19 @@
 -- Global Keymaps
 local map = vim.keymap.set
--- Normal Mode Keymaps
+
 -- Buffer Mode Keymaps
 map("n", "<leader>bb", "<cmd>b#<cr>", { desc = "Previous buffer" })
 map("n", "<leader>bd", "<cmd>bd<cr>", { desc = "Delete current buffer" })
+
+-- Split Window Keymaps
+map("n", "<Tab>", "<C-w>w", { desc = "Next window" })
+
 -- Code Keymaps
 map("n", "cd", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Show code diagnostics" })
 
 -- Visual Mode Keymaps
--- Stay in visual mode after re-indenting
 map("v", "<", "<gv", { noremap = true, silent = true })
 map("v", ">", ">gv", { noremap = true, silent = true })
 
 -- Terminal Mode Keymaps
-map('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
