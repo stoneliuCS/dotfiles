2dcce9c125f2fc1fa90dd552068e54d9fd38cab7
Stone Liu <liu.sto@northeastern.edu>
2026-02-08 19:55:02 -0500

    Finish up

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,19 +7,19 @@ vim.cmd([[colorscheme rose-pine]])
 local coq = require "coq"
 
 -- Load the lsps
-vim.lsp.config("luals", require("config.lsp.luals"))
-vim.lsp.config("texlab", require("config.lsp.texlab"))
-vim.lsp.config("basedpyright", require("config.lsp.basedpyright"))
-vim.lsp.config("pyright", require("config.lsp.pyright"))
-vim.lsp.config("vtsls", require("config.lsp.vtsls"))
-vim.lsp.config("dockerls", require("config.lsp.dockerls"))
-vim.lsp.config("yamlls", require("config.lsp.yamlls"))
-vim.lsp.config("gopls", require("config.lsp.gopls"))
-vim.lsp.config("rust-analyzer", require("config.lsp.rust-analyzer"))
-vim.lsp.config("tinymist", require("config.lsp.tinymist"))
-vim.lsp.config("templ", require("config.lsp.templ"))
-vim.lsp.config("ts_go_ls", require("config.lsp.ts_go_ls"))
-vim.lsp.config("tailwindcss", require("config.lsp.tailwindcss"))
+vim.lsp.config("luals", coq.lsp_ensure_capabilities(require("config.lsp.luals")))
+vim.lsp.config("texlab", coq.lsp_ensure_capabilities(require("config.lsp.texlab")))
+vim.lsp.config("basedpyright", coq.lsp_ensure_capabilities(require("config.lsp.basedpyright")))
+vim.lsp.config("pyright", coq.lsp_ensure_capabilities(require("config.lsp.pyright")))
+vim.lsp.config("vtsls", coq.lsp_ensure_capabilities(require("config.lsp.vtsls")))
+vim.lsp.config("dockerls", coq.lsp_ensure_capabilities(require("config.lsp.dockerls")))
+vim.lsp.config("yamlls", coq.lsp_ensure_capabilities(require("config.lsp.yamlls")))
+vim.lsp.config("gopls", coq.lsp_ensure_capabilities(require("config.lsp.gopls")))
+vim.lsp.config("rust-analyzer", coq.lsp_ensure_capabilities(require("config.lsp.rust-analyzer")))
+vim.lsp.config("tinymist", coq.lsp_ensure_capabilities(require("config.lsp.tinymist")))
+vim.lsp.config("templ", coq.lsp_ensure_capabilities(require("config.lsp.templ")))
+vim.lsp.config("ts_go_ls", coq.lsp_ensure_capabilities(require("config.lsp.ts_go_ls")))
+vim.lsp.config("tailwindcss", coq.lsp_ensure_capabilities(require("config.lsp.tailwindcss")))
 
 -- Enable LSPs
 vim.lsp.enable({
